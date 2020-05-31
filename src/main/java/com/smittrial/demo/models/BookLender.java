0302c3130d0b2ebf551c1c698451fe5361780881
CaupoHelvik <helvikcaupo@gmail.com>
2020-05-31 16:26:58 +0300

    Resolves #5

---
+++ src/main/java/com/smittrial/demo/models/BookLender.java
@@ -0,0 +1,10 @@
+package com.smittrial.demo.models;
+
+import java.util.Date;
+
+public class BookLender {
+    private int id;
+    private int userId;
+    private int bookId;
+    private Date deadline;
+}
