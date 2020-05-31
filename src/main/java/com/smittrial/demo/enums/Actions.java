66c08b31442c9d4b5900897c31787c4c5a814c6a
CaupoHelvik <helvikcaupo@gmail.com>
2020-05-31 13:29:33 +0300

    Resolves #16

---
+++ src/main/java/com/smittrial/demo/enums/Actions.java
@@ -0,0 +1,11 @@
+package com.smittrial.demo.enums;
+
+public enum Actions {
+    GET_LIBRARY_SUMMARY,
+    GET_LATE_BOOK_LENDERS,
+    SEARCH_BOOK,
+    LEND_BOOK,
+    RECEIVE_BOOK,
+    ADD_USER,
+    SEARCH_BOOK_LENDER
+}
