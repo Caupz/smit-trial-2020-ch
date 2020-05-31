6b17de82ab9451489bd6832799835779f0db0903
CaupoHelvik <helvikcaupo@gmail.com>
2020-05-31 12:35:01 +0300

    Resolves #1

---
+++ src/main/java/com/smittrial/demo/SmitTrialApplication.java
@@ -0,0 +1,13 @@
+package com.smittrial.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmitTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmitTrialApplication.class, args);
+	}
+
+}
