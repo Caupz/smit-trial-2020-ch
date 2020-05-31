bdc2bb02237fc46bc15c304a347e24359fde65b5
CaupoHelvik <helvikcaupo@gmail.com>
2020-05-31 16:23:06 +0300

    Resolves #19

---
+++ src/main/java/com/smittrial/demo/models/UserRole.java
@@ -0,0 +1,11 @@
+package com.smittrial.demo.models;
+
+public class UserRole {
+    private int userId;
+    private int roleId;
+
+    public UserRole(int _userId, int _roleId) {
+        userId = _userId;
+        roleId = _roleId;
+    }
+}
