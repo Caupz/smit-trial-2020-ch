4233280d672280f5ab818ed8fccb36f6d3c09e49
CaupoHelvik <helvikcaupo@gmail.com>
2020-06-02 22:21:04 +0300

    Tried to get unit tests running... couldn't figure out what is wrong and what I had to do.

--- src/test/java/com/smittrial/demo/SmitTrialApplicationTests.java
+++ src/test/java/com/smittrial/demo/SmitTrialApplicationTests.java
@@ -1,13 +1,30 @@
 package com.smittrial.demo;
 
+import com.smittrial.demo.service.LogServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.runners.JUnit4;
+
+import static org.springframework.test.util.AssertionErrors.fail;
 
 @SpringBootTest
+/*@ContextConfiguration(classes = {
+		LogServiceImpl.class
+})*/
+//@ContextConfiguration(locations = {"/applicationContext.xml"})
+@RunWith(JUnit4.class)
+@EnableConfigurationProperties
 class SmitTrialApplicationTests {
-
 	@Test
-	void contextLoads() {
+	void testLibrarySummary() {
+		SmitTrialApplication app = new SmitTrialApplication();
+		app.getLibrarySummary();
 	}
+
 
 }
